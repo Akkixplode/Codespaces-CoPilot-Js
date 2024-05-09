32e99a65f1d81886233749c3a6ebf569873947bb
Akhil Krishna <69808928+Akkixplode@users.noreply.github.com>
2024-05-09 12:28:38 +0000

    Changes in portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  name: "Akhil Krishna",
+  title: "Web Designer & Full Stack Developer",
+  email: "uakhilkrishna@gmail.com",
   gitHub: "microsoft",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "linkedin.com/in/akhilkrishnau",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
